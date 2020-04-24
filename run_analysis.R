0ea4b9a2e303ef5e614a4d47035526a2325f2d63
Dmitry Kozhevnikov <d.a.kozhevnikov@yandex.ru>
2020-04-24 10:42:02 +0300

    Code changed

--- run_analysis.R
+++ run_analysis.R
@@ -19,8 +19,25 @@ url_dat <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%2
 download.file(url_dat, file.path(wdir, "Data.zip"))
 unzip("Data.zip")
 
-# Loading features and labels data:
-features <- read_file(file.path(wdir, "UCI HAR Dataset", "features.txt"), col_names = c("index", "features"), delim = " ")
-labels <- read
+# Loading the features and labels data:
+feature <- data.table::fread(file.path(wdir, "UCI HAR Dataset", "features.txt"), col.names = c("ID", "featureName"))
+labels <- data.table::fread(file.path(wdir, "UCI HAR Dataset", "activity_labels.txt"), col.names = c("activityClass", "activityName"))
 
-        
+# Loading the training data:
+trainSet <- data.table::fread(file.path(wdir, "UCI HAR Dataset", "train", "X_train.txt"), col.names = feature$featureName)
+trainLabels <- data.table::fread(file.path(wdir, "UCI HAR Dataset", "train", "y_train.txt"), col.names = "activityClass")
+trainSubject <- data.table::fread(file.path(wdir, "UCI HAR Dataset", "train", "subject_train.txt"), col.names = "subjectID")
+trainData <- cbind(trainLabels, trainSubject, trainSet)
+
+# Loading the test data:
+testSet <- data.table::fread(file.path(wdir, "UCI HAR Dataset", "test", "X_test.txt"), col.names = feature$featureName)
+testLabels <- data.table::fread(file.path(wdir, "UCI HAR Dataset", "test", "y_test.txt"), col.names = "activityClass")
+testSubject <- data.table::fread(file.path(wdir, "UCI HAR Dataset", "test", "subject_test.txt"), col.names = "subjectID")
+testData <- cbind(testLabels, testSubject, testSet)
+
+# Merding the training and test datasets:
+joinData <- rbind(trainData, testData)
+
+# Extracting the measurements on the mean and standard deviation for each measurement
+
+
