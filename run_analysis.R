1ba6bb24828e3a0cb6bfac3a7b901655ecf7974f
Dmitry Kozhevnikov <d.a.kozhevnikov@yandex.ru>
2020-04-24 08:20:57 +0300

    C

--- run_analysis.R
+++ run_analysis.R
@@ -10,12 +10,17 @@
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average 
 #    of each variable for each activity and each subject.
 
-# Loading Packages for the data tidying
+# Loading Packages for the data tidying and setting the work direcory:
 library(tidyverse)
+wdir <- getwd()
 
 # Loading and getting the Data:
 url_dat <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(url_dat, "Data.zip")
-unzip("Data.zip", exdir = "Data")
+download.file(url_dat, file.path(wdir, "Data.zip"))
+unzip("Data.zip")
 
+# Loading features and labels data:
+features <- read_file(file.path(wdir, "UCI HAR Dataset", "features.txt"), col_names = c("index", "features"), delim = " ")
+labels <- read
 
+        
